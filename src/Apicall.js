4aaaeaeaed7c0cd81efb303ebfa1c2a3da56da87
hardik gogdani <hardikgogdani989@gmail.com>
2020-12-10 15:37:05 +0530

    change with rest api calling

--- src/Apicall.js
+++ src/Apicall.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Row, Col, Card, Form, Input, Button} from 'antd';
+import {Row, Col, Card, Form, Input, Button,message,Popconfirm} from 'antd';
 import Table from "antd/lib/table";
 import axios from 'axios';
 import {UserOutlined} from "@ant-design/icons";
@@ -7,13 +7,13 @@ import {UserOutlined} from "@ant-design/icons";
 
 let editedId = null;
 const ApiCall = () => {
-    const [userDetail,setUserDetail] = useState({
-        id:"",
-        employee_name:"",
-        employee_salary:"",
-        employee_age:""
+    const [userDetail, setUserDetail] = useState({
+        id: "",
+        name: "",
+        salary: "",
+        age: ""
     });
-    const [data,setData] = useState([]);
+    const [data, setData] = useState([]);
     useEffect(() => {
         listDelete();
     })
@@ -23,126 +23,134 @@ const ApiCall = () => {
         setUserDetail({...userDetail, [name]: value})
     }
 
-    const  onSubmit=()=>{
-        if(editedId !== null)
-        axios.put(`http://dummy.restapiexample.com/api/v1/update/${editedId}`,userDetail).then(response => setUserDetail(data[editedId]));
+    const onSubmit = () => {
+        if (editedId !== null) {
+            axios.put(`http://localhost:8080/notes/${editedId}`, userDetail).then(response => console.log(response));
+        } else {
+            userDetail.id = data.length + 1;
+            axios.post('http://localhost:8080/notes',{...userDetail}).then(response =>{
+
+            })
+        }
+        setUserDetail({})
     }
 
-    const listDelete = () => {
-        axios.get(`http://dummy.restapiexample.com/api/v1/employees`).then(response => setData(response.data.data || [])).catch(error => console.log(error));
+        const listDelete = () => {
+            axios.get(`http://localhost:8080/notes`).then(response => setData(response.data || [])).catch(error => console.log(error));
 
-    }
-    // const onDelete = (record) => {
-    //     axios.remove(`http://dummy.restapiexample.com/api/v1/delete/ ${record}`).then(response => {
-    //         message.success(response.data.status)
-    //     }).catch(error => console.log(error));
-    //     listDelete();
-    // }
-    const onEdit = (id) => {
-        editedId = id;
-        const findIndex = data.find(record => record.id === (id));
-        setUserDetail(findIndex)
-        listDelete();
-    }
+        }
+        const onDelete = (record) => {
+            axios.delete(`http://localhost:8080/notes/${record}`).then(response => {
+                listDelete();
+                message.success("successfully deleted")
+            }).catch(error => console.log(error));
+        }
+        const onEdit = (id) => {
+            editedId = id;
+            const findIndex = data.find(record => record._id === (id));
+            setUserDetail(findIndex)
+            listDelete();
 
-
-    const columns = [{
-        title: "id",
-        dataIndex: 'id',
-        key: 'id',
-    },
-        {
-            title: "employee_name",
-            dataIndex: "employee_name",
-            key: "employee_name",
-        },
-        {
-            title: "employee_salary",
-            dataIndex: "employee_salary",
-            key: "employee_salary",
-        },
-        {
-            title: "employee_age",
-            dataIndex: "employee_age",
-            key: "employee_age",
-        },
-        {
-            title: 'Action',
-            render: (text, record) => (
-                <div>
-                    <button className="btn btn-outline-primary btn-mini" onClick={() => {
-                        onEdit(record.id)
-                    }}>
-                        Edit
-                    </button>
-                    &nbsp; &nbsp;
-
-                    {/*<Popconfirm placement="rightTop" title={text1} onConfirm={() => {*/}
-                    {/*    onDelete(record)*/}
-                    {/*}} okText="Yes" cancelText="No">*/}
-                    {/*    <button className="btn btn-outline-danger btn-mini">*/}
-                    {/*        Delete*/}
-                    {/*    </button>*/}
-                    {/*</Popconfirm>*/}
-                </div>
-            )
-        },
-    ];
+        }
 
 
-    return (
-        <>
-            <Row >
-                <Col span={8}/>
-                <Col span={8}>
-                    <Card>
-                        <h2 style={{textAlign: "center"}}>Registration Form</h2>
-                        <p style={{textAlign: "center"}}>Creat Your Account</p><br/>
-                        <Form>
-                            <Form.Item>
-                                <Input placeholder="Enter Your firstname" name="employee_name" value={userDetail.employee_name}
-                                       onChange={handleChange} addonBefore={(<UserOutlined/>)}/>
+        const columns = [
+            {
+                title: "name",
+                dataIndex: "name",
+                key: "name",
+            },
+            {
+                title: "salary",
+                dataIndex: "salary",
+                key: "salary",
+            },
+            {
+                title: "age",
+                dataIndex: "age",
+                key: "age",
+            },
+            {
+                title: 'Action',
+                render: (text, record) => (
+                    <div>
+                        <button className="btn btn-outline-primary btn-mini" onClick={() => {
+                            onEdit(record._id)
+                        }}>
+                            Edit
+                        </button>
+                        &nbsp; &nbsp;
 
-                            </Form.Item>
-
-                            <Form.Item>
-                                <Input placeholder="Enter Your salary" name="employee_salary" value={userDetail.employee_salary}
-                                       onChange={handleChange} addonBefore={(<UserOutlined/>)}/>
-
-                            </Form.Item>
-
-                            <Form.Item>
-                                Age <Input placeholder="Enter Your age" name="employee_age" value={userDetail.employee_age}
-                                           onChange={handleChange} />
-
-                            </Form.Item>
-
-                            <Form.Item>
-                                <Button  className="btn-create-account" Type="submit" onClick={onSubmit}>
-                                    Create Account
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </Card>
-                </Col>
-                <Col span={8}/>
-            </Row>
-            <Row>
-
-                <Col span={4}/>
-                <Col span={16} className="mt-3">
-                    <Table
-                        columns={columns}
-                        dataSource={data}
-                        pagination={{pageSize: 5}}
-                    />
+                        <Popconfirm placement="rightTop"  onConfirm={() => {
+                            onDelete(record._id)
+                        }} okText="Yes" cancelText="No">
+                            <button className="btn btn-outline-danger btn-mini">
+                                Delete
+                            </button>
+                        </Popconfirm>
+                    </div>
+                )
+            },
+        ];
 
 
-                </Col>
+        return (
+            <>
+                <Row>
+                    <Col span={8}/>
+                    <Col span={8}>
+                        <Card>
+                            <h2 style={{textAlign: "center"}}>Registration Form</h2>
+                            <p style={{textAlign: "center"}}>Creat Your Account</p><br/>
+                            <Form>
+                                <Form.Item>
+                                    <Input placeholder="Enter Your firstname" name="name"
+                                           value={userDetail.name}
+                                           onChange={handleChange} addonBefore={(<UserOutlined/>)}/>
 
-            </Row>
-        </>
-    );
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <Input placeholder="Enter Your salary" name="salary"
+                                           value={userDetail.salary}
+                                           onChange={handleChange} addonBefore={(<UserOutlined/>)}/>
+
+                                </Form.Item>
+
+                                <Form.Item>
+                                    Age <Input placeholder="Enter Your age" name="age"
+                                               value={userDetail.age}
+                                               onChange={handleChange}/>
+
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <Button className="btn-create-account" Type="submit" onClick={onSubmit}>
+                                        Create Account
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Card>
+                    </Col>
+                    <Col span={8}/>
+                </Row>
+                <Row>
+
+                    <Col span={4}/>
+                    <Col span={16} className="mt-3">
+                        <Table
+                            columns={columns}
+                            dataSource={data || []}
+                            pagination={{pageSize: 5}}
+                        />
+
+
+                    </Col>
+
+                </Row>
+            </>
+        );
+
 }
 
-export default ApiCall;
+    export default ApiCall;
